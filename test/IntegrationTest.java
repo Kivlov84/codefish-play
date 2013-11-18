9e9c272ce7fc49c1a45e7a27f869ced715c97278
Clemens Helm <clemens.helm@gmail.com>
2013-11-18 14:20:24 +0100

    Initial commit

---
+++ test/IntegrationTest.java
@@ -0,0 +1,34 @@
+import org.junit.*;
+
+import play.mvc.*;
+import play.test.*;
+import play.libs.F.*;
+
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
+public class IntegrationTest {
+
+    @Test
+    public void testEnteredCodeship() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+            public void invoke(TestBrowser browser) {
+                browser.goTo("http://localhost:3333");
+                assertThat(browser.find("h1").getText()).contains("I've entered the Codeship");
+            }
+        });
+    }
+
+    @Test
+    public void testLinkToCodeship() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+            public void invoke(TestBrowser browser) {
+                browser.goTo("http://localhost:3333");
+                assertThat(browser.find("a[href='https://www.codeship.io']").getText()).contains("codeship.io");
+            }
+        });
+    }
+
+}
